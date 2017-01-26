e35338a71981041404855c3e720fd376bd4b7fad
Kaspar <kaspar.suvi@khk.ee>
2017-01-26 14:16:09 +0200

    J7_LetsUseOurFramework

--- J2_StructuringSafeCode/app.js
+++ J2_StructuringSafeCode/app.js
@@ -1,6 +1,23 @@
 /**
  * Created by Kaspar on 1/26/2017.
  */
+
+// Teeb uue objekti(sisenditega eesnimi=John, perenimi=Doe), ei pea kasutama "new" kuna Greetr arhitektuur lubab seda
 var g = G$('John', 'Doe');
 
+// use our chainable methods
 g.greet().setLang('es').greet(true).log();
+
+// let's use our object on the click of the login button
+$('#login').click(function() {
+
+    // create a new 'Greetr' object (let's pretend we know the name from the login)
+    var loginGrtr = G$('John', 'Doe');
+
+    // hide the login on the screen
+    $('#logindiv').hide();
+
+    // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log the welcome as well
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+
+});
