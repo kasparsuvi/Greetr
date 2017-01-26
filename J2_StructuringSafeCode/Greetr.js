7dc5caf458ea21a2bdbc29a375c6bab702d5eed2
Kaspar <kaspar.suvi@khk.ee>
2017-01-26 09:04:25 +0200

    J3_OurObjectAndPrototype

--- J2_StructuringSafeCode/Greetr.js
+++ J2_StructuringSafeCode/Greetr.js
@@ -3,7 +3,28 @@
  */
 //Loob IIFE, et ükski teine raamistik ei kirjutaks funktsioone üle
 (function(global, $) {
+    // uus objekt
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
 
+    //loob prototüübi, et hoida kokku mälu
+    Greetr.prototype = {};
+    // Siin tehakse tegelik objekt, nii saab ülevalpool luua uue objekti ilma "new" kasutamata
+    Greetr.init = function(firstName, lastName, language) {
 
+        var self = this;
+        //firstname, on default ''
+        self.firstName = firstName || '';
+        //lastname, on default ''
+        self.lastName = lastName || '';
+        //langauge, on default en
+        self.language = language || 'en';
+
+    }
+    // Jqueryst laenatud trikk, tänu millele ei pea kasutama "new"
+    Greetr.init.prototype = Greetr.prototype;
+    //Lisab Greetr globaalsele objektile, ning saab välja kutsuda G$ lühendiga
+    global.Greetr = global.G$ = Greetr;
 
 }(window, jQuery));
--- J2_StructuringSafeCode/app.js
+++ J2_StructuringSafeCode/app.js
@@ -1,3 +1,5 @@
 /**
  * Created by Kaspar on 1/26/2017.
  */
+var g = G$('John', 'Doe');
+console.log(g);
